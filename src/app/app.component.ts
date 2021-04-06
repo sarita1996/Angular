452bf5aaf032c5e1d3b479c20eef7406423490b9
Sarita <upreti1sarita@gmail.com>
2021-04-06 14:23:49 -0400

    simple validation implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,14 @@
-import { Component } from '@angular/core';
+import {​​​​​​​​ Component }​​​​​​​​ from'@angular/core';
+import {​​​​​​​​ User }​​​​​​​​ from'./user';
+ 
+@Component({​​​​​​​​
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
+}​​​​​​​​)
+export class AppComponent {​​​​​​​​
+title='alcorproject1';
+userModel = new User("","","","","","","","","");
 
+}​​​​​​​​
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-
-  title = 'LatestApplication';
-  //define the function
-  
-  countryList: Array<any> = [
-    { name: 'Germany', cities: ['Duesseldorf', 'Leinfelden-Echterdingen', 'Eschborn'] },
-    { name: 'Spain', cities: ['Barcelona'] },
-    { name: 'USA', cities: ['Downers Grove'] },
-    { name: 'Mexico', cities: ['Puebla'] },
-    { name: 'China', cities: ['Beijing'] },
-  ];
-
-}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {MatDatepickerModule,MatNativeDateModule} from '@angular/material';
-
-// import this to use ng model
-import {FormsModule,ReactiveFormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {MatInputModule} from '@angular/material/input';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-
-@NgModule({
-  declarations: [
-    AppComponent,
+import {​​​​​​​​ NgModule }​​​​​​​​ from'@angular/core';
+import {​​​​​​​​ FormsModule }​​​​​​​​ from'@angular/forms';
+import {​​​​​​​​ BrowserModule }​​​​​​​​ from'@angular/platform-browser';
  
+import {​​​​​​​​ AppRoutingModule }​​​​​​​​ from'./app-routing.module';
+import {​​​​​​​​ AppComponent }​​​​​​​​ from'./app.component';
+ 
+@NgModule({​​​​​​​​
+declarations: [
+AppComponent
   ],
-  //add imports here as well based on the module used in HTML
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatInputModule,
-    FormsModule,
-    MatDatepickerModule,MatNativeDateModule,
-    MatSlideToggleModule,
-    ReactiveFormsModule,
-    MatSelectModule 
+imports: [
+BrowserModule,
+FormsModule,
+AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+providers: [],
+bootstrap: [AppComponent]
+}​​​​​​​​)
+export class AppModule {​​​​​​​​ }​​​​​​​​
